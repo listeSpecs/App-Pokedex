a04892d9f7f0f223c67b97211bd2077d47dfdd4f
Caio Liste <73594006+listeSpecs@users.noreply.github.com>
2021-06-09 15:43:54 -0300

    novidades

--- src/screens/CadastroPokemon/CadastroPokemon.js
+++ src/screens/CadastroPokemon/CadastroPokemon.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { useAlert } from 'react-alert';
 import Dropzone from 'react-dropzone';
-import { fetchElementos, postPokemons } from '../../api/endpoints';
+import { fetchElementos, fetchRegioes, postPokemons } from '../../api/endpoints';
 import Botao from '../../components/Botao/Botao';
+import Carregar from '../../components/Carregar/Carregar';
 import GridBotao from '../../components/GridBotao/GridBotao';
 import {
-  AppBody, AreaForm, ArquivoForm, CampoForm, Container, Divisor, FormFooter, Grid, SelectForm,
+  Alerta,
+  AppBody, AreaForm, ArquivoForm, CampoForm, Container, Divisor, FormFooter, Grid, Linha,
+  SelectForm,
 } from '../../styles/base';
 import { espacamento } from '../../styles/constants/sizes';
 
@@ -13,26 +17,73 @@ const defaultValues = {
   descricao: '',
   imagem: '',
   elemento: '',
-  elementoCampo: '',
   subElemento: '',
-  subElementoCampo: '',
+  fracoContra1: '',
+  fracoContra2: '',
+  fracoContra3: '',
+  regiao: '',
 };
 
 const CadastroPokemon = () => {
   const [values, setValues] = useState(defaultValues);
   const [listaElementos, setListaElementos] = useState(null);
+  const [listaRegioes, setListaRegioes] = useState(null);
   const [foto, setFoto] = useState(null);
+
+  const alert = useAlert();
+
+  const validate = () => {
+    if (!values.nome) {
+      alert.error('Verifique todos os campos obrigatórios.', { timeout: 5000 });
+      return false;
+    }
+    if (!values.descricao) {
+      alert.error('Verifique todos os campos obrigatórios.', { timeout: 5000 });
+      return false;
+    }
+
+    if (!values.imagem) {
+      alert.error('Verifique todos os campos obrigatórios.', { timeout: 5000 });
+      return false;
+    }
+
+    if (!values.elemento) {
+      alert.error('Verifique todos os campos obrigatórios.', { timeout: 5000 });
+      return false;
+    }
+
+    if (!values.regiao) {
+      alert.error('Verifique todos os campos obrigatórios.', { timeout: 5000 });
+      return false;
+    }
+
+    if (!values.regiao) {
+      alert.error('Verifique todos os campos obrigatórios.', { timeout: 5000 });
+      return false;
+    }
+
+    if (!values.fracoContra1 && !values.fracoContra2 && !values.fracoContra3) {
+      alert.error('Verifique todos os campos obrigatórios.', { timeout: 5000 });
+      return false;
+    }
+
+    return true;
+  };
 
   useEffect(() => {
     Promise.all([
       fetchElementos(),
-    ]).then(([dadosElementos]) => {
+      fetchRegioes(),
+    ]).then(([dadosElementos, dadosRegioes]) => {
       setListaElementos(dadosElementos);
+      setListaRegioes(dadosRegioes);
     });
   }, []);
 
-  if (!listaElementos) {
-    return null;
+  if (!listaElementos || !listaRegioes) {
+    return (
+      <Carregar />
+    );
   }
 
   const uploadFoto = (url, options) => {
@@ -69,27 +120,66 @@ const CadastroPokemon = () => {
   };
 
   const cadastrarPokemon = async () => {
-    await postPokemons(values);
+    if (!validate()) {
+      return;
+    }
+
+    await postPokemons({
+      nome: values.nome,
+      imagem: values.imagem,
+      descricao: values.descricao,
+      elemento: {
+        id: values.elemento,
+      },
+      subElemento: {
+        id: values.subElemento,
+      },
+      fracoContra: [
+        values.fracoContra1
+          ? {
+            id: values.fracoContra1,
+          } : null,
+        values.fracoContra2
+          ? {
+            id: values.fracoContra2,
+          } : null,
+        values.fracoContra3
+          ? {
+            id: values.fracoContra3,
+          } : null,
+      ],
+      regiao: {
+        id: values.regiao,
+      },
+
+    });
 
     setValues(defaultValues);
     setFoto(null);
     alert('Pokemon cadastrado com sucesso');
   };
 
-  console.log(values);
-
   return (
     <AppBody>
 
       <Container>
         <h2>Cadastro de Pokemon</h2>
+
+        <Linha style={{ justifyContent: 'flex-end' }}>
+          <b><Alerta>* Campos obrigatórios</Alerta></b>
+        </Linha>
+
         <div style={{ borderBottom: '1px solid #cc0000', padding: espacamento.extraPequeno }}>
           <b>Cadastro</b>
         </div>
 
         <Divisor />
 
-        <b>Nome do Pokemon</b>
+        <b>
+          Nome do Pokemon
+          {' '}
+          <Alerta>*</Alerta>
+        </b>
         <CampoForm
           id="campo_nomePokemon"
           value={values.nome}
@@ -103,7 +193,11 @@ const CadastroPokemon = () => {
 
         <Divisor />
 
-        <b>Descrição do Pokemon</b>
+        <b>
+          Descrição do Pokemon
+          {' '}
+          <Alerta>*</Alerta>
+        </b>
         <AreaForm
           id="campo_descricaoPokemon"
           value={values.descricao}
@@ -117,15 +211,18 @@ const CadastroPokemon = () => {
 
         <Divisor />
 
-        <b>Elemento do Pokemon</b>
+        <b>
+          Elemento do Pokemon
+          {' '}
+          <Alerta>*</Alerta>
+        </b>
         <SelectForm
           id="elemento"
-          value={values.elementoCampo}
+          value={values.elemento}
           onChange={({ target }) => {
             setValues({
               ...values,
-              elemento: { id: Number(target.value) },
-              elementoCampo: Number(target.value),
+              elemento: Number(target.value),
             });
           }}
         >
@@ -140,18 +237,99 @@ const CadastroPokemon = () => {
         <b>Sub-elemento do Pokemon</b>
         <SelectForm
           id="select_subElemento"
-          value={values.subElementoCampo}
+          value={values.subElemento}
           onChange={({ target }) => {
             setValues({
               ...values,
-              subElemento: { id: Number(target.value) },
-              subElementoCampo: Number(target.value),
+              subElemento: Number(target.value),
             });
           }}
         >
           <option value="">&nbsp;</option>
           {listaElementos && listaElementos.map(({ id, nomeElemento }) => (
             <option key={id} value={id}>{nomeElemento}</option>
+          ))}
+        </SelectForm>
+
+        <Divisor />
+
+        <b>
+          Fraquezas do Pokemon (escolha de uma a três)
+          {' '}
+          <Alerta>*</Alerta>
+        </b>
+        <SelectForm
+          id="select_fracoContra1"
+          value={values.fracoContra1}
+          onChange={({ target }) => {
+            setValues({
+              ...values,
+              fracoContra1: Number(target.value),
+            });
+          }}
+        >
+          <option value="">&nbsp;</option>
+          {listaElementos && listaElementos.map(({ id, nomeElemento }) => (
+            <option key={id} value={id}>{nomeElemento}</option>
+          ))}
+        </SelectForm>
+
+        <Divisor />
+
+        <SelectForm
+          id="select_fracoContra2"
+          value={values.fracoContra2}
+          onChange={({ target }) => {
+            setValues({
+              ...values,
+              fracoContra2: Number(target.value),
+            });
+          }}
+        >
+          <option value="">&nbsp;</option>
+          {listaElementos && listaElementos.map(({ id, nomeElemento }) => (
+            <option key={id} value={id}>{nomeElemento}</option>
+          ))}
+        </SelectForm>
+
+        <Divisor />
+
+        <SelectForm
+          id="select_fracoContra3"
+          value={values.fracoContra3}
+          onChange={({ target }) => {
+            setValues({
+              ...values,
+              fracoContra3: Number(target.value),
+            });
+          }}
+        >
+          <option value="">&nbsp;</option>
+          {listaElementos && listaElementos.map(({ id, nomeElemento }) => (
+            <option key={id} value={id}>{nomeElemento}</option>
+          ))}
+        </SelectForm>
+
+        <Divisor />
+
+        <b>
+          Região do Pokemon
+          {' '}
+          <Alerta>*</Alerta>
+        </b>
+        <SelectForm
+          id="select_regiao"
+          value={values.regiao}
+          onChange={({ target }) => {
+            setValues({
+              ...values,
+              regiao: Number(target.value),
+            });
+          }}
+        >
+          <option value="">&nbsp;</option>
+          {listaRegioes && listaRegioes.map(({ id, nomeRegiao }) => (
+            <option key={id} value={id}>{nomeRegiao}</option>
           ))}
         </SelectForm>
 
